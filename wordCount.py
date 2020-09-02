58d98605d3be01024a443ae07d6ab90cbd99419f
JuanGarciaReyes <45839913+JuanGarciaReyes@users.noreply.github.com>
2020-09-02 00:52:54 -0600

    Update wordCount.py

--- wordCount.py
+++ wordCount.py
@@ -1,29 +1,34 @@
 import re
 
+def store(counter):
+    with open('declaration.txt','r') as file: 
+        # reading each line     
+        for line in file: 
+            line = re.sub(r'[^\w\s]', '', line)
+            line = line.lower()
+            # reading each word   
+            for word in line.split(): 
+                # displaying the words            
+                if(word in counter):
+                    counter[word] += 1
+                else:
+                    counter.update( {word : 1} )
+    return counter
+
+
+def write(counter):
+    #sorting the dict
+    sortedDict = dict( sorted(counter.items(), key=lambda x: x[0].lower()) )
+    #creating the file
+    f = open("declarationKey2.txt", "w")
+    #trasverse and write
+    for keys,values in sortedDict.items():
+        f.write('{} {}'.format(keys,values))
+        f.write('\n')
+    f.close() 
+    #done
+    print("done!"+'\n')
+    
 counter ={}
-# define punctuation
-punctuations = "._-()"
-with open('declaration.txt','r') as file: 
-    # reading each line     
-    for line in file: 
-        line = re.sub(r'[^\w\s]', '', line)
-        line = line.lower()
-        # reading each word   
-        for word in line.split(): 
-            # displaying the words            
-            if(word in counter):
-                counter[word] += 1
-            else:
-                if('-' in word or '.' in word or ',' in word):
-                    word = word[-1]
-                counter.update( {word : 1} )
-sortedDict = dict( sorted(counter.items(), key=lambda x: x[0].lower()) )
-
-f = open("declarationKey2.txt", "w")
-for keys,values in sortedDict.items():
-    f.write('{} {}'.format(keys,values))
-    f.write('\n')
-
-f.close() 
-print(counter)
-
+counter = store(counter)
+write(counter)
